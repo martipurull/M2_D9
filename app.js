9ba359e33d1d3daa78db3c85a399ef0edf69e7cc
martipurull <91315241+martipurull@users.noreply.github.com>
2021-10-14 16:51:28 +0200

    completed all exercises

---
+++ app.js
@@ -0,0 +1,26 @@
+// const addEventListenerToImages = function () {
+//     let albumImages = document.getElementsByClassName('card-img-top')
+//     for (let i = 0; i < albumImages.length; i++) {
+//         albumImages[i].addEventListener('click', toggleAlbumDetails)
+//     }
+//     console.log(albumImages)
+// }
+
+// const toggleAlbumDetails = function () {
+//     let albumCards = document.querySelectorAll('card')
+
+
+// }
+
+const deleteTrack = function (e) {
+    let buttonId = e.target.id
+    let songRows = document.querySelectorAll('tr')
+    console.log(e)
+    for (let i = 0; i < songRows.length; i++) {
+        let songNumber = songRows[i].innerText.slice(0, 2)
+        console.log(songNumber)
+    }
+}
+
+
+
